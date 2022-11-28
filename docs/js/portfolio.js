4e25f007efc7d13efb90eb5152a760277cab980b
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2022-11-28 10:56:27 -0700

    Portfolio Update

--- docs/js/portfolio.js
+++ docs/js/portfolio.js
@@ -15,29 +15,39 @@ document.addEventListener("click", function(event){
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function stop(){
+	clearInterval(interval);
+}
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
+    //Stop auto slideshow
+    stop();
 }
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    //Stop auto slideshow
+    stop();
 }
 
 function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    //if (n > slides.length) {slideIndex = 1};
-    //if (n < 1) {slideIndex = slides.length};
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length};
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
-    //for (i = 0; i < dots.length; i++) {
-    //    dots[i].className = dots[i].className.replace(" active", "");
-    //}
-    //slides[slideIndex -1].style.display = "block";
-    //dots[slideIndex -1].className += " active";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex -1].className += " active";
+}
+//Slideshow interval: stopping the slideshow on click of image
+var interval=setInterval(function(){slideIndex+=1; showSlides(slideIndex);}, 4000);
+var slides = document.getElementsByClassName("mySlides");
+for (var i = 0; i < slides.length; i++) {
+    slides[i].onclick = stop;
 }
