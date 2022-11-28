8032751e2ed80385eb6f34159c582ee991213437
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2022-11-28 08:49:24 -0700

    Portfolio Update

--- docs/js/portfolio.js
+++ docs/js/portfolio.js
@@ -26,14 +26,18 @@ function currentSlide(n) {
 function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
+    //if (n > slides.length) {slideIndex = 1};
+    //if (n < 1) {slideIndex = slides.length};
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex -1].style.display = "block";
-    dots[slideIndex -1].className += " active";
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
+    //for (i = 0; i < dots.length; i++) {
+    //    dots[i].className = dots[i].className.replace(" active", "");
+    //}
+    //slides[slideIndex -1].style.display = "block";
+    //dots[slideIndex -1].className += " active";
 }
